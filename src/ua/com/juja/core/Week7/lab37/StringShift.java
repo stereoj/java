9add0cf0695073f65a4f2c15d4b05affb5d2bc09
lukanin <ext-andrii.lukanin@here.com>
2016-09-02 22:18:32 +0300

    lab37

---
+++ src/ua/com/juja/core/Week7/lab37/StringShift.java
@@ -0,0 +1,57 @@
+package ua.com.juja.core.Week7.lab37;
+
+/**
+ * Created in IntelliJ IDEA
+ * by stereoj
+ * Date: 02.09.2016
+ * Time: 20:53
+ */
+public class StringShift {
+    public static String rightShift(String arg, int delta) {
+
+        String string1,string2;
+
+        if (arg == "") {
+            return "";
+        }
+
+        if (delta == Integer.MIN_VALUE) {
+            delta = 2;
+        }
+
+        if (delta >= 0) {
+            delta = delta % arg.length();
+
+            string1 = arg.substring(arg.length() - delta, arg.length());
+            string2 = arg.substring(0, arg.length() - delta);
+
+            System.out.println("string1 = " + string1);
+            System.out.println("string2 = " + string2);
+        }
+        else {
+            delta = -delta;
+            delta = delta % arg.length();
+
+            string1 = arg.substring(delta, arg.length());
+            string2 = arg.substring(0, delta);
+
+            System.out.println("string1 = " + string1);
+            System.out.println("string2 = " + string2);
+        }
+
+        return string1 + string2;
+    }
+}
+
+class Test {
+    public static void main(String[] args) {
+        String actual = StringShift.rightShift("0123456789", Integer.MIN_VALUE);
+        String expected = "8901234567";
+
+        if (!actual.equals(expected)) {
+            throw new AssertionError(actual);
+        }
+
+        System.out.print("OK");
+    }
+}
